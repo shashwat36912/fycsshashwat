be9854aca7b0f6f669ee727f2e8d6d40b851ee46
shashwat36912 <147688351+shashwat36912@users.noreply.github.com>
2023-10-12 11:31:08 +0530

    Add files via upload

---
+++ shashwat45.py
@@ -0,0 +1,13 @@
+sum = lambda n1,n2:n1+n2
+print("total=",sum(13,20))
+print("total=",sum(21,40))
+
+g=lambda x:x**2
+print("square of anumber:",g(8))
+
+nums = range(2,40)
+for i in range(2,8):
+              nums = filter(lambda x:x ==i
+                            or x%1,nums)
+d=lambda x:x*2
+print(d(5))
